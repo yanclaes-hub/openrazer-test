bd2882755620b7177d2d56b1cf77ee751b6a6791
yanclaes-hub <yanclaes@googlemail.com>
2025-09-03 17:44:26 +0200

    Update keyboards.py

--- daemon/openrazer_daemon/hardware/keyboards.py
+++ daemon/openrazer_daemon/hardware/keyboards.py
@@ -396,6 +396,30 @@ class RazerBlackWidowV3ProWireless(RazerBlackWidowV3ProWired):
 
     DEVICE_IMAGE = "https://dl.razerzone.com/src/3809-1-EN-v1.png"
 
+class RazerBlackWidowV4TKL_HS(_RippleKeyboard):
+    """
+    Class for the Razer BlackWidow V4 TKL
+    """
+    EVENT_FILE_REGEX = re.compile(r'.*RazerBlackWidowV4TKL_HS_(-if01)?-event-kbd')
+
+    USB_VID = 0x1532
+    USB_PID = 0x02A5
+    HAS_MATRIX = True
+    WAVE_DIRS = (1, 2)
+    MATRIX_DIMS = [6, 16]
+    POLL_RATES = [125, 250, 500, 1000]
+    METHODS = ['get_device_type_keyboard', 'set_wave_effect', 'set_wheel_effect', 'set_static_effect', 'set_spectrum_effect',
+               'set_reactive_effect', 'set_none_effect', 'set_breath_random_effect', 'set_breath_single_effect', 'set_breath_dual_effect',
+               'set_custom_effect', 'set_key_row', 'get_game_mode', 'set_game_mode', 'get_macro_mode', 'set_macro_mode',
+               'get_macro_effect', 'set_macro_effect', 'get_macros', 'delete_macro', 'add_macro',
+               'set_starlight_random_effect', 'set_starlight_single_effect', 'set_starlight_dual_effect',
+               'get_poll_rate', 'set_poll_rate', 'get_supported_poll_rates',
+               'set_ripple_effect', 'set_ripple_effect_random_colour',
+              # Battery
+               'set_charge_effect', 'set_charge_colour', 'get_battery', 'is_charging', 'get_low_battery_threshold', 'set_low_battery_threshold']
+
+    DEVICE_IMAGE = "https://dl.razerzone.com/src2/15142/15142-1-en-v1.png"
+
 
 class RazerBlackWidowChroma(_RippleKeyboard):
     """
